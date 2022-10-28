53bc913c0904f6fcc9eac9cf57512ebce16569ef
PraisesPJMT <praisesmusa@outlook.com>
2022-10-28 17:47:49 +0100

    Integrate `Links` component to react app - Add `Links` jsx component to `App.js` - Modify styles for `App` component at `Index.css`

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Profile from './components/Profile';
+import Links from './components/Links';
 
 function App() {
   return (
-    <Profile />
+    <>
+      <Profile />
+      <Links />
+    </>
   );
 }
 
