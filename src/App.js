31254d9fa08037882e7a8f0ec3635d01f3c387a2
PraisesPJMT <praisesmusa@outlook.com>
2022-10-28 18:30:14 +0100

    Integrate `Social` component to react app - Add `Social` jsx component to `App.js` - Modify styles for `App` component at `App.css`

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Profile from './components/Profile';
 import Links from './components/Links';
+import Social from './components/Social';
 
 function App() {
   return (
     <>
-      <Profile />
-      <Links />
+      <main>
+        <Profile />
+        <Links />
+        <Social />
+      </main>
     </>
   );
 }
